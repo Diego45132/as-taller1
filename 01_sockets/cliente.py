50bc3931d71befe451119c586cee27b3c8dcb729
Diego45132 <diego212294@gamil.com>
2025-08-05 08:45:31 -0500

    import

--- 01_sockets/cliente.py
+++ 01_sockets/cliente.py
@@ -1,1 +1,12 @@
 # Cliente
+import socket
+
+HOST = '127.0.0.1'
+PORT = 65432        
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hola, servidor!')
+    data = s.recv(1024)
+
+print(f"Mensaje del servidor: {data.decode('utf-8')}")
